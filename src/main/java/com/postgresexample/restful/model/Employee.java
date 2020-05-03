554507ec0fd07f53e6cc72855650a93cc5f80b6c
Tushar Sharma <tushar123cse@gmail.com>
2020-05-03 18:07:56 +0530

    RESTful example CRUD operations with SQL storage

---
+++ src/main/java/com/postgresexample/restful/model/Employee.java
@@ -0,0 +1,37 @@
+package com.postgresexample.restful.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor()
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+}
